30520f72f144676ac746312b7f0aee7f17cb434b
Priyal2807 <priyalsobti@gmail.com>
2022-06-18 22:53:41 +0530

    adding a lot of features in this microservice

--- comments/index.js
+++ comments/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
@@ -10,15 +11,40 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
     console.log(commentsByPostId);
 })
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
+    console.log(req.body)
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status:"pending" });
     commentsByPostId[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id,
+            status:'pending'
+        }
+    })
     res.status(201).send(comments);
 })
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
 
+    if (type === 'CommentModerated') {
+        const { id, status, content, postId } = data;
+        const comments = commentsByPostId[postId]
+        comments.filter(cmt => cmt.id === id).map(cmt => cmt.status = status);
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,status, postId, content
+            }
+        })
+    }
+    res.send({});
+})
 app.listen(4001, () => {
     console.log('listening on 4001');
 })
---
+++ query/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const posts = {};
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = {id,title, comments:[]}
+    }
+    if (type === 'CommentCreated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content, status }); 
+    }
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
+        const post = posts[postId]; 
+        post.comments.filter(cmt => cmt.id === id).map(cmt => cmt.status = status);
+        post.comments.filter(cmt => cmt.id === id).map(cmt => cmt.content = content);
+        console.log(post.comments)
+    }
+    res.send({});
+});
+app.listen(4002, () => {
+    console.log('listening on 4002')
+})
